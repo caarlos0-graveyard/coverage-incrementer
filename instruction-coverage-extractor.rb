523e771d5c9e55be0b8c35788d3c2a819d40079c
Carlos A Becker <caarlos0@gmail.com>
2015-02-04 18:23:35 -0200

    refactory

--- instruction-coverage-extractor.rb
+++ instruction-coverage-extractor.rb
@@ -1,20 +1,34 @@
 require 'nokogiri'
 
-def jacoco(doc)
-  node = doc.at_xpath('//report/counter[@type="INSTRUCTION"]')
-  missed = node.attributes["missed"].value.to_f
-  covered = node.attributes["covered"].value.to_f
-  (covered / (missed + covered)).round 2
+
+class Pom
+  def initialize(doc)
+    @doc = doc
+  end
+
+  def instructions
+    @doc.remove_namespaces!
+    @doc.xpath("//coverage.it.rate").text.to_f
+  end
 end
 
-def pom(doc)
-  doc.remove_namespaces!
-  doc.xpath("//coverage.it.rate").text.to_f
+class Jacoco
+  def initialize(doc)
+    @doc = doc
+  end
+
+  def instructions
+    node = @doc.at_xpath('//report/counter[@type="INSTRUCTION"]')
+    missed = node.attributes["missed"].value.to_f
+    covered = node.attributes["covered"].value.to_f
+    (covered / (missed + covered)).round(2)
+  end
 end
 
 doc = Nokogiri::XML(File.open(ARGV[1]))
-if ARGV[0].eql? "-jacoco"
-  puts jacoco(doc)
-elsif ARGV[0].eql? "-pom"
-  puts pom(doc)
-end
+parser = if ARGV[0].eql? "-jacoco"
+                  Jacoco.new(doc)
+                elsif ARGV[0].eql? "-pom"
+                  Pom.new(doc)
+                end
+puts parser.instructions
